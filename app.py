b476f8083d75ee417be365f827b1e5311bc6cb2e
moatez amairi <35851660+M-AMAIRI@users.noreply.github.com>
2023-12-31 02:58:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,8 @@
 # -*- coding: utf-8 -*-
 from flask import *
-from werkzeug.utils import secure_filename
-
-from flask_bootstrap import Bootstrap
-from urllib.parse import *
-import sqlite3, hashlib, os, time, random
 
 app = Flask(__name__)
-app.secret_key = 'dev'
-UPLOAD_FOLDER = 'static/uploads'
-ALLOWED_EXTENSIONS = set(['jpeg', 'jpg', 'png', 'gif'])
 
-Bootstrap(app)
 
 @app.route('/Generator')
 def Generator():
